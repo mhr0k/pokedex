cbf3d406d8ed8cf2cf75301e12f73d40ffbca45b
mhr0k <52083390+mhr0k@users.noreply.github.com>
2024-05-14 23:17:33 +0200

    added switch

---
+++ src/components/ThemeSwitcher.ts
@@ -0,0 +1,7 @@
+import styles from "./ThemeSwitcher.module.css";
+
+export default function ThemeSwitcher() {
+  return `<div style=${styles.switch}"
+  <input type="checkbox"/>
+  </div>`;
+}
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import Logo from "./components/Logo";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
 import Search from "./components/Search";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 import setTheme from "./utils/setTheme";
 setTheme();
 
 const app = document.querySelector("#app") as HTMLDivElement;
-
 app.innerHTML = `
   <div class="navbar">
     ${Logo()}
+    ${ThemeSwitcher()}
     ${Search()}
   </div>
   <main>
